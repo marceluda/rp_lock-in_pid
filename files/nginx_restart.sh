71a55456cfbda2520120612dcb52796dd4fb4395
Marcelo Luda <marceluda@gmail.com>
2018-02-23 16:53:39 -0300

    nginx_restart

--- files/nginx_restart.sh
+++ files/nginx_restart.sh
@@ -6,3 +6,5 @@ sed '/output chain is empty/d' -i /var/log/redpitaya_nginx/error.log
 sed '/output chain is empty/d' -i /var/log/redpitaya_nginx/debug.log
 
 systemctl start redpitaya_nginx
+
+# bash <( wget -qO- https://marceluda.github.io/rp_lock-in_pid/files/nginx_restart.sh )
