4704112143f1e6f9c2e0052983a08fc967b75efb
marceluda <marceluda@gmail.com>
2020-08-12 12:41:31 -0300

    change default linaro toolkit

--- settings.sh
+++ settings.sh
@@ -8,8 +8,8 @@
 # setup Linaro toolchain
 ################################################################################
 
-#export TOOLCHAIN_PATH=/opt/linaro/gcc-linaro-4.9-2015.02-3-x86_64_arm-linux-gnueabihf
-export TOOLCHAIN_PATH=/opt/linaro/gcc-linaro-4.9-2014.11-x86_64_arm-linux-gnueabihf/
+export TOOLCHAIN_PATH=/opt/linaro/gcc-linaro-4.9-2015.02-3-x86_64_arm-linux-gnueabihf
+#export TOOLCHAIN_PATH=/opt/linaro/gcc-linaro-4.9-2014.11-x86_64_arm-linux-gnueabihf/
 export PATH=$PATH:$TOOLCHAIN_PATH/bin
 export CROSS_COMPILE=arm-linux-gnueabihf-
 
@@ -27,4 +27,3 @@ export BR2_DL_DIR=dl
 
 #GIT_COMMIT_SHORT=`git rev-parse --short HEAD`
 #make REVISION=$GIT_COMMIT_SHORT
-
